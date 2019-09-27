90430ac0b84ea8ee37964e47fe0b9a14eea9862a
sean <zz771016958@163.com>
2019-09-27 10:54:11 +0800

    nginx-cutlog

---
+++ nginx_log.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+date=`date +%Y%m%d`
+logpath=/usr/local/nginx/logs
+mv ${logpath}/access.log ${logpath}/access-${date}.log
+mv ${logpath}/error.log ${logpath}/error-${date}.log
+kill -USR1 $(cat ${logpath}/nginx.pid) 
+
