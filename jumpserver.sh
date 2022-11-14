bb708b057632427cc74fe1d64a3d665778ff8cb9
Sean-HH <53120458+Sean-HH@users.noreply.github.com>
2022-11-14 14:28:42 +0800

    Create jumpserver.sh
    first commit

---
+++ jumpserver.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+trap "" HUP INT OUIT TSTP
+clear
+while :
+do
+  cat <<-EOF
+  1. control-plane
+  2. jenkins
+  3. nfs
+  EOF
+  read -p "Input Number: " num
+  case $num in
+  1)
+    echo "login control-plane";;
+  2)
+    echo "login jenkins";;
+  3)
+    echo "login nfs";;
+  *)
+    echo "unkown host"
+done
